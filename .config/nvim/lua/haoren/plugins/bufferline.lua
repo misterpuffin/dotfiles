4ca26533d40a021b4d237ba9567681c05ed9f873
misterpuffin <snghaoren@gmail.com>
2022-12-06 09:04:38 +0800

    Initial Commit

---
+++ .config/nvim/lua/haoren/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+-- import bufferline plugin safely
+local status, bufferline = pcall(require, "bufferline")
+if not status then
+  return
+end
+
+bufferline.setup({
+  options = {
+    numbers = "none",
+    diagnostics = "nvim_lsp",
+    separator_style = "slant" or "padded_slant",
+    show_tab_indicators = true,
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+  },
+})
