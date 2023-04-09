495a93e9e0f06de7f83691f04e3e3426e212a3fd
misterpuffin <snghaoren@gmail.com>
2023-04-09 10:52:26 +0800

    Add lualine

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,24 @@
+local Plugin = { 'nvim-lualine/lualine.nvim' }
+
+Plugin.name = 'lualine'
+
+Plugin.event = 'VeryLazy'
+
+-- See :help lualine.txt
+Plugin.opts = {
+  options = {
+    theme = 'onenord',
+    icons_enabled = true,
+    component_separators = '|',
+    section_separators = '',
+    disabled_filetypes = {
+      statusline = { 'NvimTree' }
+    }
+  },
+}
+
+function Plugin.init()
+  vim.opt.showmode = false
+end
+
+return Plugin
