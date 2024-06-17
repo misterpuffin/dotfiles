4376f2b390f08116fe8c8fbd9104cdf1faa25d20
misterpuffin <snghaoren@gmail.com>
2024-06-17 23:44:39 +0800

    Remove unused config files

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
-vim.keymap.set("n", "<C-t>", '<CMD>lua require("FTerm").toggle()<CR>', { desc = "Terminal" })
-vim.keymap.set("t", "<C-t>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-
-vim.keymap.set("n", "y", '"cy', { noremap = true })
-vim.keymap.set("n", "yy", '"cyy', { noremap = true })
