e609bfa964f487b6830af2f026164792c95bb177
misterpuffin <snghaoren@gmail.com>
2025-09-20 10:32:18 +0800

    neovim: Add Neogit and Diffview

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -61,5 +61,15 @@ return {
       end,
     },
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "folke/snacks.nvim", -- optional
+    },
+  }
 }
 -- vim: ts=2 sts=2 sw=2 et
